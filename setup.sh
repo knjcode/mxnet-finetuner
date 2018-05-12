5eb6d14dab5e05ad7d56728cf939a19d852e4140
knjcode <knjcode@gmail.com>
2018-05-12 21:56:40 +0900

    Support nvidia-docker version 2

--- setup.sh
+++ setup.sh
@@ -14,20 +14,29 @@ if ! which jq > /dev/null 2>&1; then
   exit 1
 fi
 
-if which wget > /dev/null 2>&1; then
-  config=$(wget -qO- 'http://localhost:3476/docker/cli/json')
-else
-  if which curl > /dev/null 2>&1; then
-    config=$(curl -s 'http://localhost:3476/docker/cli/json')
-  else
-    echo 'wget and curl commands not found.' 1>&2
-    exit 1
-  fi
+# check nvidia-docker version
+if nvidia-docker > /dev/null 2>&1; then
+  NVIDIA_DOCKER_VERSION=$(nvidia-docker version|head -n1|cut -d' ' -f3|cut -d'.' -f1)
 fi
 
-VOLUMES=($(echo "$config" | jq -r ".Volumes | .[]"))
-ExistDEV=$(echo "$config" | jq .Devices)
-DEVICES=($(echo "$config" | jq -r ".Devices | .[]"))
+### for nvidia-docker ver 1.x
+if [[ $NVIDIA_DOCKER_VERSION != "2" ]]; then
+  if which wget > /dev/null 2>&1; then
+    config=$(wget -qO- 'http://localhost:3476/docker/cli/json')
+  else
+    if which curl > /dev/null 2>&1; then
+      config=$(curl -s 'http://localhost:3476/docker/cli/json')
+    else
+      echo 'wget and curl commands not found.' 1>&2
+      exit 1
+    fi
+  fi
+
+  VOLUMES=($(echo "$config" | jq -r ".Volumes | .[]"))
+  ExistDEV=$(echo "$config" | jq .Devices)
+  DEVICES=($(echo "$config" | jq -r ".Devices | .[]"))
+fi
+### for nvidia-docker ver 1.x end
 
 # Generate docker-compose.yml according to your environment
 if [ -e docker-compose.yml ]; then
@@ -35,8 +44,8 @@ if [ -e docker-compose.yml ]; then
   read -r ANS
   case $ANS in
     "Y" | "y" | "yes" | "Yes" | "YES" )
-      generate_compose "$CUR_DIR" "$DOCKER_COMPOSE_FILE" \
-      && update_compose "$CUR_DIR" "$DEVICES" "$DOCKER_COMPOSE_FILE"
+      generate_compose "$CUR_DIR" "$DOCKER_COMPOSE_FILE" "$NVIDIA_DOCKER_VERSION" \
+      && update_compose "$CUR_DIR" "$DEVICES" "$DOCKER_COMPOSE_FILE" "$NVIDIA_DOCKER_VERSION"
       ;;
     * )
       echo "not overwritten" 1>&2
